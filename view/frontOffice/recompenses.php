aaf10f6b31e03388f8df2a85f9e26b4b6c486e32
Eya <Eya.Rahmani@esprit.tn>
2025-04-28 02:11:21 +0100

    seventh commit

--- view/frontOffice/recompenses.php
+++ view/frontOffice/recompenses.php
@@ -23,6 +23,9 @@ function isPromo($dateFin) {
     <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="promo-styles.css" />
 </head>
+<script>
+(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="N9VVpJ57n-tJQCTWSzGgc";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
+</script>
 <body>
     <header class="main-header">
         <nav class="nav-container">
